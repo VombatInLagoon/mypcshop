3439c5abf2bbca7270a7a519bfbdcb69d1f22bbe
amin.khorsandi <amin.khorsandi@37b8db49-fdaf-41fd-5063-b093b4e147c8>
2011-06-12 18:45:07 +0000

    This might be the final commit!

--- trunk/web/dbSql/setup.sh
+++ trunk/web/dbSql/setup.sh
@@ -5,6 +5,6 @@ username=root # you need to provide the username here
 pwd=sesame #here you need to enter password
 host=localhost #here hostame
 
-mysql -u $username -p $pwd -D $db < setup.sql
+mysql -u $username -p < setup.sql
 
 # a user name "root" with password "sesame" must exist in the database system!
